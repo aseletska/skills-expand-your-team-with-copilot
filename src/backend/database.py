0dc8ef879d4767deb416b7beee6979e6077f8b94
Alyona Seletska <128473738+aseletska@users.noreply.github.com>
2026-06-28 18:09:03 +0300

    Update Manga Maniacs description and schedule

--- src/backend/database.py
+++ src/backend/database.py
@@ -165,14 +165,14 @@ initial_activities = {
         "participants": ["william@mergington.edu", "jacob@mergington.edu"]
     },
     "Manga Maniacs": {
-        "description": "Dive into the vibrant world of Japanese Manga! From epic shonen battles to heartwarming slice-of-life tales, we read, discuss, and celebrate the art of manga storytelling together.",
-        "schedule": "Tuesdays, 7:00 PM",
+        "description": "Step into an action-packed world of heroes, rivals, and epic story arcs as you explore the fantastic characters and adventures of Japanese manga.",
+        "schedule": "Tuesdays, 5:00 PM",
         "schedule_details": {
             "days": ["Tuesday"],
-            "start_time": "19:00",
-            "end_time": "20:00"
+            "start_time": "17:00",
+            "end_time": "18:00"
         },
-        "max_participants": 15,
+        "max_participants": 25,
         "participants": []
     }
 }
